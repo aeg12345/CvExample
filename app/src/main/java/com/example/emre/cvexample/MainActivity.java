b3f49d43913bd2f37ea24ee8839297cca6677935
aeg12345 <aliemregursu@gmail.com>
2016-05-17 00:00:37 +0300

    14444444444444444444444444444

--- app/src/main/java/com/example/emre/cvexample/MainActivity.java
+++ app/src/main/java/com/example/emre/cvexample/MainActivity.java
@@ -20,6 +20,9 @@ import android.util.Log;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 
@@ -28,21 +31,19 @@ import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
 
     ImageView sonucimg;
 
 
-
-
-static {
-        if (!OpenCVLoader.initDebug()){
+    static {
+        if (!OpenCVLoader.initDebug()) {
             Log.i("opencv", "opencv initialzation failed");
-        }
-        else {
+        } else {
             Log.i("opencv", "opencv initialzation success");
         }
 
@@ -52,28 +53,23 @@ static {
     private static final String TAG = "OCVSample::Activity";
 
 
-
-
-
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
+                case LoaderCallbackInterface.SUCCESS: {
                     Log.i(TAG, "OpenCV loaded successfully");
-                   // mOpenCvCameraView.enableView();
-                  //  mOpenCvCameraView.setOnTouchListener(MainActivity.this);
-                } break;
-                default:
-                {
+                    // mOpenCvCameraView.enableView();
+                    //  mOpenCvCameraView.setOnTouchListener(MainActivity.this);
+                }
+                break;
+                default: {
                     super.onManagerConnected(status);
-                } break;
+                }
+                break;
             }
         }
     };
-
-
 
 
     @Override
@@ -81,9 +77,9 @@ static {
         Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         sonucimg = (ImageView) findViewById(R.id.sonucimg);
+        sonucimg = (ImageView) findViewById(R.id.sonucimg);
 
-        new MatchingDemo().run(R.drawable.kafam, R.drawable.uzak, R.drawable.sonuc, Imgproc.TM_SQDIFF_NORMED);
+        new MatchingDemo().run(Imgproc.TM_SQDIFF_NORMED);
 
 
 
@@ -101,35 +97,35 @@ static {
 
 
     public class MatchingDemo {
-        public void run(int inFile, int templateFile, int outFile, int match_method) {
+        public void run(int match_method) {
             System.out.println("\nRunning Template Matching");
 
             Mat img = new Mat();
-            Mat templ= new Mat();
+            Mat templ = new Mat();
 
             System.out.println("\nRead the images");
             /*templ= Imgcodecs.imread(imgf);
                 img=Imgcodecs.imread(tf);*/
 
             try {
-                img = Utils.loadResource(getApplicationContext(), R.drawable.uzak, Imgcodecs.CV_LOAD_IMAGE_COLOR);
-                templ = Utils.loadResource(getApplicationContext(), R.drawable.kafam, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+                img = Utils.loadResource(getApplicationContext(), R.drawable.ontl2, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+                templ = Utils.loadResource(getApplicationContext(), R.drawable.ontl1, Imgcodecs.CV_LOAD_IMAGE_COLOR);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            System.out.println("\nRead the images"+templ);
-            System.out.println("\nRead the images"+img);
+            System.out.println("\nRead the images" + templ);
+            System.out.println("\nRead the images" + img);
 
             // / Create the result matrix
             int result_cols = img.cols() - templ.cols() + 1;
             int result_rows = img.rows() - templ.rows() + 1;
             Mat result = new Mat(result_rows, result_cols, CvType.CV_8UC3);
             System.out.println("\nRead the columns");
-            System.out.println("\nRead the column"+result_cols);
+            System.out.println("\nRead the column" + result_cols);
             System.out.println("\nRead the rows" + result_rows);
             // / Do the Matching and Normalize
             Imgproc.matchTemplate(img, templ, result, match_method);
-
+            //Imgproc.matchTemplate(img,templ,result,Imgproc.TM_CCOEFF_NORMED);
             //Core.normalize(result, result, 0, 1, Core.NORM_MINMAX, -1, new Mat());
             System.out.println("\nMatch template run");
             System.out.println("\nResult" + result);
@@ -142,7 +138,7 @@ static {
             } else {
                 matchLoc = mmr.maxLoc;
             }
-            Mat Temp=new Mat(result_rows,result_cols, CvType.CV_8UC3);
+            Mat Temp = new Mat(result_rows, result_cols, CvType.CV_8UC3);
             System.out.println("\nResults temp mat" + Temp);
             //result.convertTo(Temp, CvType.CV_8UC4);
             System.out.println("\nResults temp mat" + Temp);
@@ -155,24 +151,91 @@ static {
                     matchLoc.y + templ.rows()), new Scalar(0, 255, 0));
 
             // Save the visualized detection.
-            System.out.println("Writing " + outFile);
-            System.out.println("Writing " + result.channels());
-            System.out.println("\nRead the images"+templ);
-            System.out.println("\nRead the images"+img);
 
 
+            System.out.println("\nRead the images" + templ);
+            System.out.println("\nRead the images" + img);
+            double d= correlation(templ, img);
+
+            System.out.println("\nThe correlation of the images" +String.format("%.2f", d) );
             //Imgcodecs.imwrite(oF, result);
             Bitmap resultBitmap = Bitmap.createBitmap(img.cols(), img.rows(), Bitmap.Config.ARGB_8888);
 
-            Utils.matToBitmap(img,resultBitmap);
+            Utils.matToBitmap(img, resultBitmap);
             sonucimg.setImageBitmap(resultBitmap);
         }
     }
 
 
+    double correlation(Mat image_1, Mat image_2) {
+        Mat src_base =new Mat();
+        Mat hsv_base=new Mat();
+        Mat src_test1=new Mat();
+        Mat hsv_test1=new Mat();
 
 
+        try {
+            src_base = Utils.loadResource(getApplicationContext(), R.drawable.ontl2, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+            src_test1 = Utils.loadResource(getApplicationContext(), R.drawable.ontl1, Imgcodecs.CV_LOAD_IMAGE_COLOR);
 
 
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        Imgproc.cvtColor(src_base, hsv_base, Imgproc.COLOR_BGR2HSV);
+        Imgproc.cvtColor(src_test1, hsv_test1, Imgproc.COLOR_BGR2HSV);
 
+        /// Using 50 bins for hue and 60 for saturation
+        int hBins = 50;
+        int sBins = 60;
+        MatOfInt histSize = new MatOfInt( hBins,  sBins);
+
+        // hue varies from 0 to 179, saturation from 0 to 255
+        MatOfFloat ranges =  new MatOfFloat( 0f,180f,0f,256f );
+
+        // we compute the histogram from the 0-th and 1-st channels
+        MatOfInt channels = new MatOfInt(0, 1);
+
+
+        Mat histRef = new Mat();
+        Mat histSource = new Mat();
+
+        ArrayList<Mat> histImages=new ArrayList<Mat>();
+        histImages.add(hsv_base);
+        Imgproc.calcHist(histImages,
+                channels,
+                new Mat(),
+                histRef,
+                histSize,
+                ranges,
+                false);
+        Core.normalize(histRef,
+                histRef,
+                0,
+                1,
+                Core.NORM_MINMAX,
+                -1,
+                new Mat());
+
+        histImages=new ArrayList<Mat>();
+        histImages.add(hsv_test1);
+        Imgproc.calcHist(histImages,
+                channels,
+                new Mat(),
+                histSource,
+                histSize,
+                ranges,
+                false);
+        Core.normalize(histSource,
+                histSource,
+                0,
+                1,
+                Core.NORM_MINMAX,
+                -1,
+                new Mat());
+        double resp1 = Imgproc.compareHist(histRef, histSource, 0);
+
+        return resp1;
+    }
 }
